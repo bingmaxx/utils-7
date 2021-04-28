883082d79fb7588be07c9c560e721092b9e6f075
bingmax <bingmaxx@163.com>
2021-04-28 17:01:47 +0800

    datetime & datetime.test

--- src/datetime.js
+++ src/datetime.js
@@ -1,21 +1,68 @@
+const isDate = function (date) {
+  if (date === null || date === undefined) return false;
+  if (isNaN(new Date(date).getTime())) return false;
+  if (Array.isArray(date)) return false; // deal with `new Date([ new Date() ]) -> new Date()`
+  return true;
+}
+
+const toDate = function (date) {
+  return isDate(date) ? new Date(date) : null;
+}
+
 /**
- * 日期格式化
+ * 日期时间转时间戳
+ * @param {*} value 日期时间
+ * @param {String} type 时间戳格式 'm', 'ms'
+ * @return {Number} res 时间戳
  */
-export const dateS = date => (date ? parseInt(new Date(date).getTime() / 1000) : parseInt(new Date().getTime() / 1000));
-export const sDate = s => (s ? new Date(s * 1000 + 28800000).toISOString().replace(/[ZT]/g, ' ').substring(0, 19) : '--');
-export const sYMD = s => (s ? new Date(s * 1000 + 28800000).toISOString().substring(0, 10).replace(/[ZT]/g, ' ') : '--');
-export const sMD = s => (s ? new Date(s * 1000 + 28800000).toISOString().substring(5, 10).replace(/[ZT]/g, ' ') : '--');
-export const dateMs = date => (date ? parseInt(new Date(date).getTime()) : parseInt(new Date().getTime()));
-export const msDate = s => (s ? new Date(s + 28800000).toISOString().replace(/[ZT]/g, ' ').substring(0, 19) : '--');
+export const date2Timestamp = (value, type) => {
+  type = type || 'm';
+  const date = toDate(value);
+  if (!date) return null;
+
+  const func = {
+    'm': () => (Math.round(date.getTime() / 1000)),
+    'ms': () => (date.getTime()),
+  }
+  if (typeof func[type] !== 'function') return null;
+  return func[type]();
+}
+
+/**
+ * 时间戳格式化
+ * 推荐的日期格式化库 https://github.com/taylorhakes/fecha
+ * @param {Number} value 时间戳
+ * @param {String} format 日期时间格式
+ * @param {String} type 时间戳格式 'm', 'ms'
+ * @return {Number} res 格式化后的日期时间
+ */
+export const timestampFormat = (value, format, type) => {
+  format = format || 'yyyy-MM-dd HH:mm:ss';
+  type = type || 'm';
+  if (typeof value !== 'number') return;
+  const timestamp = type === 'm' ? value * 1000 : value;
+  const def = '--';
+  if (!isDate(timestamp)) return def;
+
+  const func = {
+    'yyyy-MM-dd HH:mm:ss': () => (new Date(timestamp + 28800000).toISOString().substring(0, 19).replace(/[ZT]/g, ' ')),
+    'yyyy-MM-dd': () => (new Date(timestamp + 28800000).toISOString().substring(0, 10).replace(/[ZT]/g, ' ')),
+    'MM-dd': () => (new Date(timestamp + 28800000).toISOString().substring(5, 10).replace(/[ZT]/g, ' ')),
+  };
+  if (typeof func[format] !== 'function') return def;
+  return func[format]();
+}
+
 
 /**
  * 判断给定日期是本年的第几周
- * @param {Date || String} date 给定日期
+ * @param {*} date 给定日期
  * @return {Number} weeks
  */
 export const getWeeks = (date) => {
+  const now = toDate(date) || new Date();
+
   const day_ms = 24 * 3600 * 1000;
-  const now = typeof date === 'string' ? new Date(date) : date;
   const now_0_ms = now.getTime() - (now.getTime() % day_ms);
 
   const new_year = new Date(now.getFullYear(), 0, 1);
--- test/datetime.test.js
+++ test/datetime.test.js
@@ -1,5 +1,29 @@
 import assert from 'assert';
-import { getWeeks } from '../src/datetime';
+import { date2Timestamp, timestampFormat, getWeeks } from '../src/datetime';
+
+describe('date2Timestamp', function () {
+  it('normal', function () {
+    assert.strictEqual(date2Timestamp('2021-01-01'), 1609430400);
+    assert.strictEqual(date2Timestamp('2021-01-01', 'ms'), 1609430400000);
+  });
+  it('abnormal', function () {
+    assert.strictEqual(date2Timestamp(null), null);
+    assert.strictEqual(date2Timestamp(undefined), null);
+  });
+});
+
+describe('timestampFormat', function () {
+  it('normal', function () {
+    assert.strictEqual(timestampFormat(1609430400), '2021-01-01 00:00:00');
+    assert.strictEqual(timestampFormat(1609430400000, null, 'ms'), '2021-01-01 00:00:00');
+    assert.strictEqual(timestampFormat(1609430400, 'yyyy-MM-dd'), '2021-01-01');
+    assert.strictEqual(timestampFormat(1609430400, 'MM-dd'), '01-01');
+  });
+  it('abnormal', function () {
+    assert.strictEqual(timestampFormat(null), '--');
+    assert.strictEqual(timestampFormat(undefined), '--');
+  });
+});
 
 describe('getWeeks', function () {
   it('normal', function () {
@@ -7,5 +31,6 @@ describe('getWeeks', function () {
     assert.strictEqual(getWeeks('2021-01-04'), 2);
     assert.strictEqual(getWeeks('2021-04-28'), 18);
     assert.strictEqual(getWeeks(new Date('2021-01-01')), 1);
+    assert.strictEqual(getWeeks(), getWeeks(new Date()));
   });
 });
