8665cd1550658da5a8fdef701f20e79b64d97b87
bingmax <bingmaxx@163.com>
2022-04-28 11:37:36 +0800

    feat: publish.sh

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -x
+
+# argument version 'v0.0.0'
+if [[ ($# > 0) && $1 ]]; then
+  git tag $1
+  git push origin --tag
+
+  npm run build
+  npm publish --tag $1
+  echo "[SUCCESS]"
+else
+  echo "please provide argument [version]"
+fi
